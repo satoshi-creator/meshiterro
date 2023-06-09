33d90ddbff883267c5705bca1299618a1ebdf6f3
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-09 06:07:24 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :post_images, only: [:new, :create, :index, :show]
-
+  root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+     resource :favorites, only: [:create, :destroy]
+     resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
 end
